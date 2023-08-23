78fe940a90e3fca799b340e530372bd994f7b3d3
frckbrice <maebrie2017@gmail.com>
2023-08-23 20:07:26 +0100

    fixed js standard code

--- assets/script/scalebalancing.js
+++ assets/script/scalebalancing.js
@@ -1,51 +1,47 @@
-const sbform = document.querySelector("#sbform");
-const balance = document.querySelector(".balance");
-const listOfWeights = document.querySelector(".array-weight");
+const balance = document.querySelector('.balance')
 
-let result = [];
+let result = []
 
-balance.style.cursor = 'pointer';
-balance.addEventListener("click", displayscalebalance);
+balance.style.cursor = 'pointer'
+balance.addEventListener('click', displayscalebalance)
 
-const result1 = document.querySelector(".resultat");
-const precise = document.querySelector(".precise");
+const result1 = document.querySelector('.resultat')
+const precise = document.querySelector('.precise')
 
+result1.style.color = '#fff'
+result1.style.fontFamily = 'serif'
+result1.innerHTML = result
 
-result1.style.color = "#fff";
-result1.style.fontFamily = "serif";
-result1.innerHTML = result;
-
-function displayscalebalance(){
-  console.log(ScaleBalancing());
-  precise.innerHTML = ScaleBalancing();
-  result1.innerHTML = result;
+function displayscalebalance () {
+  console.log(ScaleBalancing())
+  precise.innerHTML = ScaleBalancing()
+  result1.innerHTML = result
 }
- 
-// function to balance the scale having two object weights
-function ScaleBalancing() {
 
+// function to balance the scale having two object weights
+function ScaleBalancing () {
   // to get the two elements of the balance
-  const leftWeight = parseInt(document.getElementById("lftweight").value);
-  const rightWeight = parseInt(document.getElementById("rgtweight").value);
+  const leftWeight = parseInt(document.getElementById('lftweight').value)
+  const rightWeight = parseInt(document.getElementById('rgtweight').value)
 
   // to get all the input elements
-  let NodeListe = document.querySelectorAll(".weight");
+  const NodeListe = document.querySelectorAll('.weight')
 
-  //to convert the returned Node list to an array
-  let weights = [];
-  weights = Array.prototype.slice.call(NodeListe, 0);
+  // to convert the returned Node list to an array
+  let weights = []
+  weights = Array.prototype.slice.call(NodeListe, 0)
 
-  //to convert array of string to array of integer
-  let newWeight = [];
+  // to convert array of string to array of integer
+  const newWeight = []
   for (let i = 0; i < weights.length; i++) {
-    newWeight[i] = parseInt(weights[i].value);
+    newWeight[i] = parseInt(weights[i].value)
   }
 
   // case where the two weights are equal
-  if (leftWeight == rightWeight) {
-    console.log("equal");
-    result = [leftWeight, rightWeight];
-    return "equal";
+  if (leftWeight === rightWeight) {
+    console.log('equal')
+    result = [leftWeight, rightWeight]
+    return 'equal'
   } else {
     for (let i = 0; i < newWeight.length; i++) {
       if (
@@ -53,11 +49,11 @@ function ScaleBalancing() {
         rightWeight + newWeight[i] === leftWeight
       ) {
         if (leftWeight > rightWeight) {
-          result = [newWeight[i]];
-          return "  right, add : " + newWeight[i];
+          result = [newWeight[i]]
+          return '  right, add : ' + newWeight[i]
         } else {
-          result = [newWeight[i]];
-          return "  left, add : " + newWeight[i];
+          result = [newWeight[i]]
+          return '  left, add : ' + newWeight[i]
         }
       }
     }
@@ -67,70 +63,69 @@ function ScaleBalancing() {
       for (let j = 0; j < weights.length; j++) {
         if (leftWeight === rightWeight + newWeight[i] + newWeight[j]) {
           if (newWeight[i] > newWeight[j]) {
-            result = [newWeight[j], newWeight[i]];
+            result = [newWeight[j], newWeight[i]]
           } else {
-            result = [newWeight[i], newWeight[j]];
+            result = [newWeight[i], newWeight[j]]
           }
           return (
-            " right, add the two : " + newWeight[i] + "  and : " + newWeight[j]
-          );
+            ' right, add the two : ' + newWeight[i] + '  and : ' + newWeight[j]
+          )
         } else if (rightWeight === leftWeight + newWeight[i] + newWeight[j]) {
           if (newWeight[i] > newWeight[j]) {
-            result = [newWeight[j], newWeight[i]];
+            result = [newWeight[j], newWeight[i]]
           } else {
-            result = [newWeight[i], newWeight[j]];
+            result = [newWeight[i], newWeight[j]]
           }
           return (
-            " left, add the two : " + newWeight[i] + "  and : " + newWeight[j]
-          );
+            ' left, add the two : ' + newWeight[i] + '  and : ' + newWeight[j]
+          )
         } else {
-
           // case3:  where we add one weight on both sides
           if (leftWeight + newWeight[i] === rightWeight + newWeight[j]) {
             if (leftWeight > rightWeight) {
               if (newWeight[i] > newWeight[j]) {
-                result = [newWeight[j], newWeight[i]];
+                result = [newWeight[j], newWeight[i]]
                 return (
-                  " left, add : " +
+                  ' left, add : ' +
                   newWeight[j] +
-                  ", right add : " +
+                  ', right add : ' +
                   newWeight[i]
-                );
+                )
               } else {
-                result = [newWeight[i], newWeight[j]];
+                result = [newWeight[i], newWeight[j]]
                 return (
-                  " left, add : " +
+                  ' left, add : ' +
                   newWeight[i] +
-                  ", right add : " +
+                  ', right add : ' +
                   newWeight[j]
-                );
+                )
               }
             } else {
               if (newWeight[i] > newWeight[j]) {
-                result = [newWeight[j], newWeight[i]];
+                result = [newWeight[j], newWeight[i]]
                 return (
-                  " left, add : " +
+                  ' left, add : ' +
                   newWeight[i] +
-                  ", right add : " +
+                  ', right add : ' +
                   newWeight[j]
-                );
+                )
               } else {
-                result = [newWeight[i], newWeight[j]];
+                result = [newWeight[i], newWeight[j]]
                 return (
-                  " left, add : " +
+                  ' left, add : ' +
                   newWeight[j] +
-                  ", right add : " +
+                  ', right add : ' +
                   newWeight[i]
-                );
+                )
               }
             }
           }
         }
       }
     }
-  } 
-  result = "Scale Imbalanced";
-  return "Scale Imbalanced";
+  }
+  result = 'Scale Imbalanced'
+  return 'Scale Imbalanced'
 }
 
 // test of js code
